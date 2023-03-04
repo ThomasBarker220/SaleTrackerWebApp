48bfeaf3f086d91e1ea2e71e710899610be1bdb4
ThomasBarker220 <james.barker@duke.edu>
2023-03-04 02:40:23 -0500

    Started building website with html

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+clothes = [
+    {
+        'name': 'Sweater',
+        'price': '$50',
+        'link': 'www.abercrombie.com',
+        'image': 'sweaterimage.png'
+    },
+    {
+        'name': 'Sweater',
+        'price': '$50',
+        'link': 'www.abercrombie.com',
+        'image': 'sweaterimage.png'
+    }
+]
+
 @app.route('/')
 
-def index():
-    return "This is my sale tracker web app!"
+def home():
+    return render_template('home.html')
 
+@app.route('/items')
+def items():
+    return render_template('items.html', clothes=clothes, title='Items Page')
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
