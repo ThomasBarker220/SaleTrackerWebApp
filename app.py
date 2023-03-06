81fde984ed8c99d22312a4f5bfee23ced96c1a75
ThomasBarker220 <james.barker@duke.edu>
2023-03-06 01:29:47 -0500

    Added login and registration forms

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '93e55391acfb950a54ffed2f30e5cb18'
 
 clothes = [
     {
@@ -25,6 +28,20 @@ def home():
 @app.route('/items')
 def items():
     return render_template('items.html', clothes=clothes, title='Items Page')
+
+
+@app.route('/register')
+
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+
+def login():
+    form = LoginForm()
+    return render_template('register.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
